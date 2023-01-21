4449a3f8c90e686398c9a0ccdbfcdd728dd11139
Muhammad Za'im Muzakki <112878416+Mozkyy@users.noreply.github.com>
2023-01-21 23:17:54 +0700

    Add files via upload
    Tugas 1 Biodata

---
+++ Biodata.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+main ()
+{
+                        cout<<"BIODATA"<<endl;
+
+    cout <<"NIK              : 3321090106040001" <<endl ;
+
+    cout <<"Nama             : MUHAMMAD ZA'IM MUZAKKI" <<endl;
+
+    cout <<"Tempat/Tgl Lahir : DEMAK , 01 JUNI 2004" <<endl;
+
+    cout <<"Jenis Kelamin    : Laki-laki" <<endl;
+
+    cout <<"Gol Darah       `: A" <<endl;
+
+    cout <<"Alamat           : KARANGANYAR, DEMAK" <<endl;
+
+    cout <<"RT / RW          : 03/ 04" <<endl;
+
+    cout <<"KeL/Desa         : KARANGANYAR" <<endl;
+
+    cout <<"Kecamatan        : KARANGANYAR" <<endl;
+
+    cout <<"Agama            : ISLAM" <<endl;
+
+    cout <<"Status Perkawinan: BELUM KAWIN" <<endl;
+
+    cout <<"Pekerjaan        : MAHASISWA" <<endl;
+
+    cout <<"Kewerganaan      : WNI " <<endl;
+
+    cout <<"Masa Berlaku     : SEUMUR HIDUP";
+}
