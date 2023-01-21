6457e6fd12e19c67629976abda6e9c0877ae55b6
Muhammad Za'im Muzakki <112878416+Mozkyy@users.noreply.github.com>
2023-01-22 00:56:46 +0700

    Add files via upload

---
+++ penjumlahan 2 variabel/x+y.cpp
@@ -0,0 +1,21 @@
+/*JUDUL :Membuat program penjumlahan 2 variabel
+16 September 2022
+Muhammad Za’im Muzakki */
+
+#include <iostream>
+using namespace std;
+
+//kamus
+int main()
+{
+    int x;
+    int y;
+    float hasilpenjumlahanxdany;
+
+//diskripsi
+    x = 7;
+    y = 3;
+    hasilpenjumlahanxdany = (x + y);
+    cout << hasilpenjumlahanxdany << endl;
+    return 0;
+}
